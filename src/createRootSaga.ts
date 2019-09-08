28d44cb01545aa31f29d7d8bdd4663d02a855bed
Michael Hettmer <mail@michael-hettmer.de>
2019-09-08 19:08:42 +0200

    Add restart delay option and documentation

--- src/createRootSaga.ts
+++ src/createRootSaga.ts
@@ -1,15 +1,39 @@
-import { all, spawn, call } from 'redux-saga/effects';
+import { all, spawn, call, delay } from 'redux-saga/effects';
 import { Saga } from '@redux-saga/types';
 
-const defaultErrorHandler = (e: unknown, saga: Saga) => {
+/**
+ * Default error handling implementation. Prints the thrown exception argument
+ * and the name of the crashed saga with log level 'warn'.
+ *
+ * @param e The exception argument that got thrown. Usually of type Error('message').
+ * @param saga The saga that throw the exception.
+ */
+export const defaultErrorHandler = (e: unknown, saga: Saga) => {
     console.warn(`${saga.name} has failed`, e);
 };
 
-interface Options {
+/**
+ * Options to pass to createRootSaga function to control its behavior.
+ */
+export interface Options {
+    /**
+     * Error handler that gets called when a saga throws an execption.
+     */
     errorHandler?: (e: unknown, saga: Saga) => void;
+
+    /**
+     * Delay between saga crash and saga restart. Default is 1000ms.
+     */
+    restartDelay?: number;
 }
 
-const createRootSaga = (sagas: Saga[], { errorHandler = defaultErrorHandler }: Options) => {
+/**
+ *  Create a root saga which starts and manages the execution of 1:n child sagas.
+ *
+ * @param sagas Array of child sagas the root saga should manage.
+ * @param options Specify and control the behavior of the root saga and how the execution of the child sagas is managed.
+ */
+const createRootSaga = (sagas: Saga[], { errorHandler = defaultErrorHandler, restartDelay = 1000 }: Options) => {
     return function* rootSaga() {
         yield all(
             sagas.map(saga =>
@@ -21,6 +45,7 @@ const createRootSaga = (sagas: Saga[], { errorHandler = defaultErrorHandler }: O
                         } catch (e) {
                             errorHandler(e, saga);
                         }
+                        yield delay(restartDelay);
                     }
                 }),
             ),
